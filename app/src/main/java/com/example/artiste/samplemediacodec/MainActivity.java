f8ee2328231e30f7cca75b6a9c99291170b1b80a
artistehsu <a8856034>
2015-05-25 12:53:30 +0800

    Change menu item Settings to Play This is changes for UI for play feature

--- app/src/main/java/com/example/artiste/samplemediacodec/MainActivity.java
+++ app/src/main/java/com/example/artiste/samplemediacodec/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_play) {
             return true;
         }
 
