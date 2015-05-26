9f6e08f8c14c31c50c6446c7934a11dfe21e375f
artistehsu <a8856034>
2015-05-26 09:52:49 +0800

    UI for video decoder test
    1. Change menu "Settings" to "Play"
    2. Add SurfaceView and pass Surface to SampleMediaCodec for video output

--- app/src/main/java/com/example/artiste/samplemediacodec/MainActivity.java
+++ app/src/main/java/com/example/artiste/samplemediacodec/MainActivity.java
@@ -4,14 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.Surface;
 
 public class MainActivity extends Activity {
-
+    SampleMediaCodec sampleMediaCodec;
+    private SurfaceView videoSurfaceView;
+    private SurfaceHolder surfaceHolder;
+    private Surface surface;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sampleMediaCodec = new SampleMediaCodec();
+        videoSurfaceView = (SurfaceView)findViewById(R.id.surfaceView);
+        surfaceHolder = videoSurfaceView.getHolder();
+        surface = surfaceHolder.getSurface();
     }
 
     @Override
@@ -30,6 +40,7 @@ public class MainActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_play) {
+            sampleMediaCodec.play(this, surface);
             return true;
         }
 
